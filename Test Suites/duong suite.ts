63b94f5a0853b5842c13acf23cd0418feb2f9dd1
duongdao <duongdao089@gmail.com>
2021-09-03 11:33:43 +0700

    test duong 1

---
+++ Test Suites/duong suite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>duong suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>71b11ae2-e447-4b75-9fcf-5b1a24540ff9</testSuiteGuid>
+   <testCaseLink>
+      <guid>342dc8c5-1f61-45f3-aa1f-3fbe1f2ea720</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/duong train</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bd02a066-1a8f-4b74-b6ae-f430ca55b031</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
